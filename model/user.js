5574d4c4a960bf0c858a7e99b1975c48119adb3d
Geoff Simons <geoff.simons@gmail.com>
2017-07-13 14:06:43 -0700

    Added name field

--- model/user.js
+++ model/user.js
@@ -13,7 +13,8 @@ const userSchema = Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  findHash: { type: String, unique: true }
+  findHash: { type: String, unique: true },
+  name: { type: String }
 });
 
 userSchema.methods.generatePasswordHash = function(password) {
