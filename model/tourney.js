bccbc62257bd2a25174126e1b21786bb12c9a3f0
Geoff Simons <geoff.simons@gmail.com>
2017-07-14 15:24:21 -0700

    Simplified model

--- model/tourney.js
+++ model/tourney.js
@@ -1,19 +1,19 @@
 'use strict';
 
-// const createError = require('http-errors');
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
 mongoose.Promise = Promise;
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
-// const debug = require('debug')('mnp:game');
 
 const tourneySchema = Schema({
   created: { type: Date, default: Date.now },
-  name: { type: String }, // TODO Create semi-random default
+  name: String, // TODO Create semi-random default
+  machines: [ String ],
   teams: [{ type: ObjectId, ref: 'team' }],
   rounds: [{ type: ObjectId, ref: 'round' }],
-  venue: { type: ObjectId, ref: 'venue' },
+  // venue: { type: ObjectId, ref: 'venue' },
+  venue: String,
   owner: { type: ObjectId, ref: 'user' }
 });
 
